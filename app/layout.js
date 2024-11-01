7aa45e29c0db54635ddce434ef68ebc7696fdf89
turgardens <turgenevgardens@gmail.com>
2024-11-01 14:05:51 +0200

    yandex-metrik-add

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import "./styles/button.css";
 import Header from "../components/blocks/header";
 import Footer from "../components/blocks/footer";
 import VideoPlayer from "../components/elements/videoPopup";
+import Script from "next/script";
 
 export const metadata = {
   title: "Тургеневские сады — свежие яблоки, соки, печенье и сидр из Калининграда",
@@ -21,6 +22,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
+      <head>
+        <Script id='yandexMetrik' type="text/javascript" >
+          {`   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(95547942, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });`}
+        </Script>
+      </head>
+
       <body>
         <VideoPlayer />
         <Header />
